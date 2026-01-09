13a2f15d22b3c28076b70cad0158d0c398b62dbf
Nikulin <s.b.nikuliny@gmail.com>
2026-01-09 19:37:26 +0700

    fix: main added db_healthcheck_loop

--- app/main.py
+++ app/main.py
@@ -1,22 +1,47 @@
-from collections.abc import AsyncGenerator
+import asyncio
+
+import contextlib
 from contextlib import asynccontextmanager
+from loguru import logger
+
+from collections.abc import AsyncGenerator
 
 from fastapi import APIRouter, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-from loguru import logger
 from prometheus_fastapi_instrumentator import Instrumentator
 
 from app.auth.router import router as router_auth
 from app.health import router as health_router
+from app.health import check_database_connection, database_connection_gauge
+from app.dao.database import async_session_maker
+
+
+async def db_healthcheck_loop():
+    while True:
+        logger.info("check_database_connection")
+        try:
+            async with async_session_maker() as session:
+                await check_database_connection(session)
+        except Exception as e:
+            logger.error(f"DB background healthcheck failed: {e}")
+            database_connection_gauge.set(0)
+
+        await asyncio.sleep(5)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncGenerator[None]:
     """Управление жизненным циклом приложения."""
     logger.info("Инициализация приложения...")
-    yield
-    logger.info("Завершение работы приложения...")
+    try:
+        task = asyncio.create_task(db_healthcheck_loop())
+        yield
+        logger.info("Завершение работы приложения...")
+    finally:
+        task.cancel()
+        with contextlib.suppress(asyncio.CancelledError):
+            await task
 
 
 def create_app() -> FastAPI:
@@ -49,7 +74,7 @@ def create_app() -> FastAPI:
 
     # Регистрация роутеров
     register_routers(app)
-    
+
     # Настройка Prometheus мониторинга
     instrumentator = Instrumentator()
     instrumentator.instrument(app).expose(app)
